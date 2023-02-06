0247f7642e9b6abb1e44ba240141a568dd535cb3
syukatsu <117654409+ryusei00734@users.noreply.github.com>
2023-02-06 21:37:22 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,4 +1,21 @@
 
 public class FizzBuzz {
 
+	public static void main(String[] args) {
+		int number = 0;
+		while (number < 100) {
+			number = number + 1;
+			if (number % 3 == 0 && number % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (number % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (number % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(number);
+			}
+		}
+
+	}
+
 }
